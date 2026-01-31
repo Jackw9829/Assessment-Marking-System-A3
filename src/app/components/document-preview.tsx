5af4ff9a86635bb3e5444c3ae114de4dc3277e8b
Jackw9829 <junming883@gmail.com>
2026-01-31 14:14:26 +0800

    commit

--- src/app/components/document-preview.tsx
+++ src/app/components/document-preview.tsx
@@ -4,7 +4,7 @@ import { Button } from './ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from './ui/card';
 import { Badge } from './ui/badge';
 import { toast } from 'sonner';
-import { getSubmissionPreviewUrl, isPreviewableFileType, isDocumentType } from '@/lib/supabase-helpers';
+import { getSubmissionPreviewUrl, isPreviewableFileType, isDocumentType } from '../../lib/supabase-helpers';
 
 interface DocumentPreviewProps {
     filePath: string;
